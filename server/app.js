dd4388d64ea88c29af99dc0bca306449de86ab41
waristo <waristo.dev@gmail.com>
2020-11-09 10:10:13 +0900

    route

--- server/app.js
+++ server/app.js
@@ -1,17 +1,27 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import config from './config'
+import express from "express";
+import mongoose from "mongoose";
+import config from "./config";
 
 const app = express();
-const {MONGO_URI} = config;
+const { MONGO_URI } = config;
+
+app.use(hpp());
+app.use(helmet());
+
+app.use(cors({ origin: true, credentials: true }));
+app.use(morgan("dev"));
+
+app.use(express.json());
 
 mongoose
-    .connect(MONGO_URI, {
-        useNewUrlParser : true,
-        useUnifiedTopology : true,
-    })
-    .then(()=> console.log("MongoDB connecting success!!"))
-    .catch((e)=> console.log(e));
-app.get('/');
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connecting success!!"))
+  .catch((e) => console.log(e));
+
+// Use routes
+app.get("/");
 
 export default app;
---
+++ server/routes/api/post.js
@@ -0,0 +1,30 @@
+import express from "express";
+
+// Model
+import Post from "../../models/post";
+
+const router = express.Router();
+
+//api/post
+router.get("/", async (req, res) => {
+  const postFindResult = await Post.find();
+  console.log(postFindResult, "All Post Get");
+  res.json(postFindResult);
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    console.log(req, "req");
+    const { title, contents, fileUrl, creator } = req.body;
+    const newPost = await Post.create({
+      title,
+      contents,
+      fileUrl,
+      creator,
+    });
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+export default router;
